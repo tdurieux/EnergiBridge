07a98afb97bc557dda2e7e9777dda7ee748b3672
tdurieux <durieuxthomas@hotmail.com>
2024-02-28 13:54:50 +0000

    fix: fix summary for intel CPUs

--- src/main.rs
+++ src/main.rs
@@ -125,6 +125,10 @@ fn main() {
                         let energy = results["CPU_ENERGY (J)"];
                         let old_energy = previous_results["CPU_ENERGY (J)"];
                         energy_array += old_energy - energy;
+                    } else if results.contains_key("PACKAGE_ENERGY (J)") {
+                        let energy = results["PACKAGE_ENERGY (J)"];
+                        let old_energy = previous_results["PACKAGE_ENERGY (J)"];
+                        energy_array += old_energy - energy;
                     }
                 }
                 previous_time = SystemTime::now();
