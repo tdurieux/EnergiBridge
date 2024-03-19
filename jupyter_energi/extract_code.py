a9d3f77e2f04c610adadc98906387cb3ba8286a5
mitalipatil99 <patilmitali99@gmail.com>
2024-03-19 20:19:30 +0100

    code to extract cells with markers added

---
+++ jupyter_energi/extract_code.py
@@ -0,0 +1,35 @@
+import nbformat
+
+
+def extract_and_write_code(notebook_path, start_marker, end_marker):
+    extracted_code = []
+    is_extracting = False
+    
+    with open(notebook_path, 'r', encoding='utf-8') as notebook_file:
+        notebook_content = nbformat.read(notebook_file, as_version=4)
+        
+        for cell in notebook_content.cells:
+            if cell.cell_type == 'code':
+                source_code = cell.source
+                if start_marker in source_code:
+                    is_extracting = True
+                    # Exclude the start marker 
+                    source_code = source_code.split(start_marker, 1)[-1]
+                if is_extracting:
+                    extracted_code.append(source_code)
+                if end_marker in source_code:
+                    is_extracting = False
+                    # Exclude the end marker
+                    extracted_code[-1] = extracted_code[-1].rsplit(end_marker, 1)[0]
+    
+    # Write extracted code to a new Python file(temp.py)
+    with open('jupyter_energi/temp.py', 'w', encoding='utf-8') as temp_file:
+        temp_file.write('\n'.join(extracted_code))
+
+
+#params
+notebook_path = 'jupyter_energi\Demo.ipynb'
+start_marker = '#EnergiBridgeStart'
+end_marker = '#EnergiBridgeStop'
+
+extract_and_write_code(notebook_path, start_marker, end_marker)
---
+++ jupyter_energi/temp.py
@@ -0,0 +1,6 @@
+
+from time import sleep
+
+for i in range(3):
+    sleep(1)
+    print(i)
